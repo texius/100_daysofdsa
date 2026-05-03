70da4203e78173de159515bce3f026c6a5742682
texius <spidermanps73@gmail.com>
2026-05-03 12:19:38 +0000

    Optimize and update Day 40 module

---
+++ day40/day40.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+void swap(int *a, int *b) { int t = *a; *a = *b; *b = t; }
+
+void heapify(int arr[], int n, int i) {
+    int largest = i;
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
+
+    if (l < n && arr[l] > arr[largest]) largest = l;
+    if (r < n && arr[r] > arr[largest]) largest = r;
+
+    if (largest != i) {
+        swap(&arr[i], &arr[largest]);
+        heapify(arr, n, largest);
+    }
+}
+
+void heapSort(int arr[], int n) {
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+
+    for (int i = n - 1; i > 0; i--) {
+        swap(&arr[0], &arr[i]);
+        heapify(arr, i, 0);
+    }
+}
+
+int main() {
+    int n;
+    int arr[1000];
+    
+    if (scanf("%d", &n) != 1) return 0;
+    for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
+    
+    heapSort(arr, n);
+    
+    for (int i = 0; i < n; i++) {
+        printf("%d", arr[i]);
+        if (i < n - 1) printf(" ");
+    }
+    printf("\n");
+    return 0;
+}
