079d8ec03611ce9a35556c9f280325d6ce59d692
texius <spidermanps73@gmail.com>
2026-05-05 11:56:17 +0000

    Optimize and update Day 48 module

---
+++ day48/day48.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *left, *right;
+};
+
+struct Node* createTree(int arr[], int n, int i) {
+    if (i < n && arr[i] != -1) {
+        struct Node* root = (struct Node*)malloc(sizeof(struct Node));
+        root->data = arr[i];
+        root->left = createTree(arr, n, 2 * i + 1);
+        root->right = createTree(arr, n, 2 * i + 2);
+        return root;
+    }
+    return NULL;
+}
+
+int countLeaves(struct Node* root) {
+    if (!root) return 0;
+    if (!root->left && !root->right) return 1;
+    return countLeaves(root->left) + countLeaves(root->right);
+}
+
+int main() {
+    int n;
+    int arr[1000];
+    if (scanf("%d", &n) != 1) return 0;
+    for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
+    struct Node* root = createTree(arr, n, 0);
+    printf("%d\n", countLeaves(root));
+    return 0;
+}
