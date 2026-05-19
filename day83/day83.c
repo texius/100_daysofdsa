294acbebf6f7310c8ec7eb55e52582b7317a06b1
texius <spidermanps73@gmail.com>
2026-05-19 14:33:01 +0000

    Optimize and update Day 83 module

---
+++ day83/day83.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    if (scanf("%d", &n) != 1) return 0;
+    
+    int arr[1000];
+    for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
+    
+    for (int i = 0; i < n - 1; i++) {
+        int min_idx = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min_idx]) {
+                min_idx = j;
+            }
+        }
+        int temp = arr[i];
+        arr[i] = arr[min_idx];
+        arr[min_idx] = temp;
+    }
+    
+    for (int i = 0; i < n; i++) {
+        printf("%d", arr[i]);
+        if (i < n - 1) printf(" ");
+    }
+    printf("\n");
+    
+    return 0;
+}
